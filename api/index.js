8221c1e2af073ba3d6380e23834d97172564893c
reddyshekharc55 <reddyshekharc55@gmail.com>
2023-11-02 22:19:21 +0530

    added Mongo server

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to server");
+});
 
-const port=4000
-const app=express();
+const port = 4000;
+const app = express();
 
 
-app.listen(port,()=>{
-console.log(`app is running on port:${port}`);
+app.listen(port, () => {
+    console.log(`app is running on port:${port}`);
 });
