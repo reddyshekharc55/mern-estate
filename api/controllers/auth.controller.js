8095caddb8683ebb429b9916d010fe3a37ecddc5
reddyshekharc55 <reddyshekharc55@gmail.com>
2023-11-13 21:42:09 +0530

    create a function to handle error and middlware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export  const  signup=async (req,res)=>{
+import { errorHandler } from "../utils/error.js";
+
+export  const  signup=async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword=bcrypt.hashSync(password,10)
     const newUser=new User({username,email,password:hashedPassword});
@@ -8,6 +10,6 @@ export  const  signup=async (req,res)=>{
         await newUser.save();
         res.status(201).json('user created succesfully');
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
-}
+};
--- api/index.js
+++ api/index.js
@@ -12,8 +12,17 @@ const port = 4000;
 const app = express();
 
 app.use(express.json())
-app.use("/api/user",userRouter)
-app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 app.listen(port, () => {
     console.log(`app is running on port:${port}`);
