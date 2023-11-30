334687a9894f4d4f6d73c3a7f333469fc3646d16
reddyshekharc55 <reddyshekharc55@gmail.com>
2023-11-30 21:17:37 +0530

    create sign api

--- clients/src/pages/SignUp.jsx
+++ clients/src/pages/SignUp.jsx
@@ -1,38 +1,57 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
-  const [formData,setFormData]=useState({});
-  const handleChange=(e)=>{
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]:e.target.value
+      [e.target.id]: e.target.value
     })
   }
 
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const res=await fetch('/api/auth/signup',{
-      method:'POST',
-      headers:{
-        'Content-type':'application/json'
-      },
-      body:JSON.stringify(formData)
-    });
-    console.log(`res is ${res}`);
-    const data=await res.json();
-  }
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+      console.log(data);
+    }
+    catch (error) {
+      setLoading(false);
+      console.error("loading", loading);
+      setError(error.message);
+    }
+  };
 
   return (
-    <div className='p-33 max-w-lg max-auto text-center'>
+    <div className='p-3 max-w-lg max-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
         SignUp
       </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
-        <input className='border p-3 rounded-lg' type='text' placeholder='username' id='username'  onChange={handleChange}/>
-        <input className='border p-3 rounded-lg' type='email' placeholder='email' id='email' onChange={handleChange}/>
-        <input className='border p-3 rounded-lg' type='password' placeholder='password' id='password' onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:80'>sign up</button>
+        <input className='border p-3 rounded-lg' type='text' placeholder='username' id='username' onChange={handleChange} />
+        <input className='border p-3 rounded-lg' type='email' placeholder='email' id='email' onChange={handleChange} />
+        <input className='border p-3 rounded-lg' type='password' placeholder='password' id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:80'>{loading ? 'Loading ...' : 'sign up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account</p>
@@ -40,7 +59,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-
   )
 }
